9af5f6fdd8629c790d5fa1de735984d5a2c507a9
Jason Pepas <jasonpepas@gmail.com>
2023-11-04 16:07:11 -0500

    pulling in utils, adding imdb rating to detail page

---
+++ util/mkseries.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# create an mserve.json for a tv series.
+
+title = input("title? ")
+tmdb_id = input("tmdb_id? ")
+with open("mserve.json", "w") as fd:
+    fd.write("{\n")
+    fd.write('    "type": "series",\n')
+    fd.write('    "title": "%s",\n' % title)
+    fd.write('    "tmdb_id": "%s"\n' % tmdb_id)
+    fd.write("}\n")
---
+++ util/resub.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+# use regular expressions to rename files.
+
+# example: to rename foo-season-1-episode-2.mp4 to foo-s1e2.mp4:
+# ./resub.py 'season-' 's' *.mp4
+# ./resub.py '-episode-' 'e' *.mp4
+
+import sys
+import os
+import re
+
+re1 = sys.argv[1]
+re2 = sys.argv[2]
+fnames = sys.argv[3:]
+for fname in fnames:
+    fname2 = re.sub(re1, re2, fname)
+    if fname2 != fname:
+        answer = input("Rename '%s' -> '%s'? " % (fname, fname2))
+        if answer == 'y' or answer == '':
+            os.rename(fname, fname2)
